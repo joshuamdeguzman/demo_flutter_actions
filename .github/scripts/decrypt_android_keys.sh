7ced75112e483075881018bd535e544b08e0a3f9
joshuadeguzman <joshua.maiquez.deguzman@gmail.com>
2020-08-29 10:25:27 +0800

    Update script

--- .github/scripts/decrypt_android_keys.sh
+++ .github/scripts/decrypt_android_keys.sh
@@ -3,6 +3,4 @@
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$ANDROID_KEYS_SECRET_PASSPHRASE" \
---output android/android_keys.zip android/android_keys.zip.gpg && jar xvf android/android_keys.zip
-
-ls
+--output android/android_keys.zip android/android_keys.zip.gpg && cd android && jar xvf android_keys.zip && cd -
