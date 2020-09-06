7366d9696e2ac52599a452c47d6eb229bfd369b0
joshuadeguzman <joshua.maiquez.deguzman@gmail.com>
2020-09-06 12:57:33 +0800

    Initial commit

---
+++ .github/scripts/decrypt_android_keys.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# --batch to prevent interactive command
+# --yes to assume "yes" for questions
+gpg --quiet --batch --yes --decrypt --passphrase="$ANDROID_KEYS_SECRET_PASSPHRASE" \
+--output android/android_keys.zip android/android_keys.zip.gpg && jar xvf android/android_keys.zip
